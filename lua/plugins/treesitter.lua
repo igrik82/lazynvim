cc849352a223b2b146f414f5e625a816b4d7fa98
Igor <igrik82@gmail.com>
2024-03-19 15:24:36 +0500

    Установлен плагин treesitter

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,38 @@
+return {
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local treesitter = require("nvim-treesitter.configs")
+
+		-- configure telescope
+		treesitter.setup({
+			ensure_installed = {
+				"cpp",
+				"c",
+				"python",
+				"markdown",
+				"markdown_inline",
+				"vim",
+				"vimdoc",
+				"bash",
+				"css",
+				"dockerfile",
+				"html",
+				"json",
+				"lua",
+				"sql",
+				"yaml",
+				"diff",
+				"git_rebase",
+				"gitattributes",
+				"passwd",
+				"requirements",
+				"toml",
+			},
+			auto_install = true,
+			highlight = {
+				enable = true,
+			},
+		})
+	end,
+}
