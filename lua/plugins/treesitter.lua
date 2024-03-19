f485f3fe888f5c0965da655d0cbf72be7c0fa34e
Igor <igrik82@gmail.com>
2024-03-19 15:44:22 +0500

    Автодобавление отсутствующего языка в treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,40 +1,15 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		local treesitter = require("nvim-treesitter.configs")
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    local treesitter = require("nvim-treesitter.configs")
 
-		-- configure telescope
-		treesitter.setup({
-			ensure_installed = {
-				"gitignore",
-				"arduino",
-				"cpp",
-				"c",
-				"python",
-				"markdown",
-				"markdown_inline",
-				"vim",
-				"vimdoc",
-				"bash",
-				"css",
-				"dockerfile",
-				"html",
-				"json",
-				"lua",
-				"sql",
-				"yaml",
-				"diff",
-				"git_rebase",
-				"gitattributes",
-				"passwd",
-				"requirements",
-				"toml",
-			},
-			auto_install = true,
-			highlight = {
-				enable = true,
-			},
-		})
-	end,
+    -- configure telescope
+    treesitter.setup({
+      auto_install = true,
+      highlight = {
+        enable = true,
+      },
+    })
+  end,
 }
