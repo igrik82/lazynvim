1157a6f27db9a621836abb53835ebc7bbefafe62
Igor <igrik82@gmail.com>
2024-03-19 20:15:10 +0500

    Fix which-key

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -1,27 +1,27 @@
-
 return {
-  "folke/which-key.nvim",
-  lazy = true,
-  event = "VeryLazy",
-  keys = {"<leader>", "<c-r>"},
-  cmd = "WhichKey",
-  init = function()
-    vim.o.timeout = true
-    vim.o.timeoutlen = 400
- local wk = require("which-key") 
-      local mappings = {
+	"folke/which-key.nvim",
+	lazy = true,
+	event = "VeryLazy",
+	keys = { "<leader>", "<c-r>" },
+	cmd = "WhichKey",
+	init = function()
+		vim.o.timeout = true
+		vim.o.timeoutlen = 400
+		local wk = require("which-key")
+		local mappings = {
 
-  -- Compile and run C|C++ program
-	t = {":w<CR>:belowright split |!bash ~/Projects/cpp/compile.sh %<CR>|:q |:FloatermNew ./main<CR>", "Compile & run"},
-
-
-}
-local opts = {prefix = "<leader>", "<c-r>"}
-wk.register(mappings, opts)
-  end,
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-  },
+			-- Compile and run C|C++ program
+			t = {
+				":w<CR>:belowright split |!bash ~/Projects/cpp/compile.sh %<CR>|:q |:FloatermNew ./main<CR>",
+				"Compile & run",
+			},
+		}
+		local opts = { prefix = "<leader>", "<c-r>" }
+		wk.register(mappings, opts)
+	end,
+	opts = {
+		-- your configuration comes here
+		-- or leave it empty to use the default settings
+		-- refer to the configuration section below
+	},
 }
