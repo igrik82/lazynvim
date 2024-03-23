07199d4cc02aef0361e78677c10cf259562c05d9
Igor <igrik82@gmail.com>
2024-03-23 18:16:08 +0500

    ИзмениУвеличина скорость загрузки темы GRUVEBOX-MATERIAL

--- lua/plugins/gruvbox.lua
+++ lua/plugins/gruvbox.lua
@@ -1,6 +1,12 @@
 return {
   "sainnhe/gruvbox-material",
   config = function()
-    vim.cmd.colorscheme "gruvbox-material"
-  end
+    -- Accent themes by default medium
+    -- vim.g.gruvbox_material_background = "soft"
+    -- vim.g.gruvbox_material_background = "hard"
+
+    vim.g.gruvbox_material_better_performance = 1
+
+    vim.cmd.colorscheme("gruvbox-material")
+  end,
 }
