c812446f7a6b49e6b9d948b3a80c5bbe32ec3df3
Igor <igrik82@gmail.com>
2024-03-25 13:28:58 +0500

    Выключил уведомления от Lazy.nvim

--- init.lua
+++ init.lua
@@ -8,26 +8,27 @@ require("core.options")
 --Setup lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("plugins", {
-  change_detection = {
-    notify = false,
-  },
-  install = {
-    colorscheme = { "gruvbox-material", "habamax" },
-  },
-  checker = {
-    enabled = true,
-    concurrency = 1,
-  },
+	change_detection = {
+		notify = false,
+	},
+	install = {
+		colorscheme = { "gruvbox-material", "habamax" },
+	},
+	checker = {
+		enabled = true,
+		concurrency = 1,
+		notify = false,
+	},
 })
