13e9248cc27164dec7cc03baec4ad62d4b6302a0
Igor <igrik82@gmail.com>
2024-03-28 18:02:36 +0500

    Добавил плагин MASON-TOOL-INSTALLER

---
+++ lua/plugins/mason-tool-installer.lua
@@ -0,0 +1,13 @@
+return {
+	"whoissethdaniel/mason-tool-installer.nvim",
+  event = "VeryLazy",
+	config = function()
+		require("mason-tool-installer").setup({
+			ensure_installed = {
+				"cpptools",
+			},
+			auto_update = true,
+			run_on_start = true,
+		})
+	end,
+}
