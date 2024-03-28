6f5cebab7a7d807e73e846cd1cefa90e4ccae04f
Igor <igrik82@gmail.com>
2024-03-28 08:21:27 +0500

    Переключение актмвности Codeium по клавишам Ctrl + x

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -1,6 +1,9 @@
 return {
 	"Exafunction/codeium.vim",
 	event = "InsertEnter",
+  keys = {
+    {"<C-x>", ":CodeiumToggle<CR>", desc = "Codeium toggle"},
+  },
 	config = function()
 		vim.keymap.set("i", "<C-a>", function()
 			return vim.fn["codeium#Accept"]()
@@ -8,10 +11,7 @@ return {
 		vim.keymap.set("i", "<C-c>", function()
 			return vim.fn["codeium#CycleCompletions"](1)
 		end, { expr = true, silent = true })
-		vim.keymap.set("i", "<C-x>", function()
-			return vim.fn["codeium#Clear"]()
-		end, { expr = true, silent = true })
 		-- Disable TAB
-		vim.g.codeium_no_map_tab = 1
+    vim.g.codeium_no_map_tab = 1
 	end,
 }
