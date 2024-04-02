61c0eb242115867fa05b44817ce523fd08e5d6a1
Igor <igrik82@gmail.com>
2024-04-02 10:48:18 +0500

    Изменил сочетание на leader+c

--- lua/plugins/coments.lua
+++ lua/plugins/coments.lua
@@ -1,23 +1,23 @@
 return {
 	"numToStr/Comment.nvim",
 	event = "VeryLazy",
-	init = function()
-		local wk = require("which-key")
-		local mappings = {
-			c = "Comments",
-		}
-		local opts = { prefix = "<leader>" }
-		wk.register(mappings, opts)
-	end,
+	-- init = function()
+	-- 	local wk = require("which-key")
+	-- 	local mappings = {
+	-- 		c = "Comments",
+	-- 	}
+	-- 	local opts = { prefix = "<leader>" }
+	-- 	wk.register(mappings, opts)
+	-- end,
 	opts = {
 		ignore = "^$",
 		toggler = {
-			line = "<leader>cc",
-			block = "<leader>cb",
+			line = "<leader>c",
+			-- block = "<leader>b",
 		},
 		opleader = {
-			line = "<leader>cc",
-			block = "<leader>cb",
+			line = "<leader>c",
+			-- block = "<leader>b",
 		},
 	},
 }
