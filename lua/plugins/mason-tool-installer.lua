dba46926c94f23ff437a666e10d31e71e39107dd
Igor <igrik82@gmail.com>
2024-04-06 12:08:55 +0500

    Добавлина функция определения архитектуры

--- lua/plugins/mason-tool-installer.lua
+++ lua/plugins/mason-tool-installer.lua
@@ -2,9 +2,27 @@ return {
 	"whoissethdaniel/mason-tool-installer.nvim",
 	-- event = "VeryLazy",
 	config = function()
+		-- Функция проверки архитектуры для установки cpptools
+		local packet = function()
+			local handle = io.popen("uname -m", "r")
+			if handle then
+				local arch = handle:read("*a")
+				handle:close()
+				-- Delete all unnesary symbols
+				arch = arch:gsub("\n", "")
+				if arch == "aarch64" then
+					return ""
+				else
+					-- Перечислить нужные пакеты здесь в фроматее
+					-- return "cpptools", "beautysh"
+					return "cpptools"
+				end
+			end
+		end
+
 		require("mason-tool-installer").setup({
 			ensure_installed = {
-				"cpptools",
+				packet(),
 			},
 			auto_update = true,
 			run_on_start = true,
