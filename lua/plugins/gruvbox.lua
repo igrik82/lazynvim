bb7a012161aa4c8c92e838aa5425d936b2f3495d
Igor <igrik82@gmail.com>
2024-04-07 07:43:17 +0500

    Minor updates

--- lua/plugins/gruvbox.lua
+++ lua/plugins/gruvbox.lua
@@ -1,14 +1,16 @@
 return {
-  "sainnhe/gruvbox-material",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    -- Accent themes by default medium
-    -- vim.g.gruvbox_material_background = "soft"
-    -- vim.g.gruvbox_material_background = "hard"
+	{
+		"sainnhe/gruvbox-material",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			-- Accent themes by default medium
+			-- vim.g.gruvbox_material_background = "soft"
+			-- vim.g.gruvbox_material_background = "hard"
 
-    vim.g.gruvbox_material_better_performance = 1
+			vim.g.gruvbox_material_better_performance = 1
 
-    vim.cmd.colorscheme("gruvbox-material")
-  end,
+			vim.cmd.colorscheme("gruvbox-material")
+		end,
+	},
 }
