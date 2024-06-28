e2414444f3d3bb018f7fc39e20b1e9e7246d7e89
Igor <igrik82@gmail.com>
2024-06-28 11:42:36 +0500

    Оставил данный коммит, так как были проблеммы с плагином при сохранении

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -1,11 +1,11 @@
 return {
 	"Exafunction/codeium.vim",
 	event = "InsertEnter",
-  -- after the commit :wq stop responding
-  commit = "3cc779d",
-  keys = {
-    {"<C-x>", ":CodeiumToggle<CR>", desc = "Codeium toggle"},
-  },
+	-- after the commit :wq stop responding
+	commit = "3cc779d",
+	keys = {
+		{ "<C-x>", ":CodeiumToggle<CR>", desc = "Codeium toggle" },
+	},
 	config = function()
 		vim.keymap.set("i", "<C-a>", function()
 			return vim.fn["codeium#Accept"]()
@@ -14,6 +14,6 @@ return {
 			return vim.fn["codeium#CycleCompletions"](1)
 		end, { expr = true, silent = true })
 		-- Disable TAB
-    vim.g.codeium_no_map_tab = 1
+		vim.g.codeium_no_map_tab = 1
 	end,
 }
